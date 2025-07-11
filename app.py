8aff428d45aa0e6ce09a6ee08cd77554087af8c7
Jenna Steffes <168142203+jesteffes@users.noreply.github.com>
2025-07-11 15:02:56 -0700

    Created columns for database structure

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ from flask import Flask, render_template
 
 from flask_sqlalchemy import SQLAlchemy
 
+from datetime import datetime, timezone
+
 # Initialize the Flask application
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
 db = SQLAlchemy(app)
+class Villain(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(80), unique=True, nullable=False)
+   description = db.Column(db.String(250), nullable=False)
+   interests = db.Column(db.String(250), nullable=False)
+   url = db.Column(db.String(250), nullable=False)
+   date_added = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+
+   def _repr_(self):
+      return "<Villain "+ self.name + ">"
 
 @app.route("/")
 def hello_world():
